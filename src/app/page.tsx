b0f9b296243280a91a7b9150e6339451e2af1a07
Olha Chornei <Chornei8053@gmail.com>
2025-12-17 22:54:10 +0200

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,14 @@ import DirectionsSection from '../components/ui/DirectionsSection';
 import HowWeWork from '../components/ui/HowWeWork';
 import WhyUs from '../components/ui/WhyUs';
 import Footer from '../components/ui/Footer';
+import BoardSlider from '../components/ui/BoardSlider';
 
 const LandingPage: React.FC = () => {
   return (
     <div className="min-h-screen font-inter overflow-x-hidden">
       <Header />
       <HeroSection />
+      <BoardSlider />
       <AboutSection />
       <DirectionsSection />
       <HowWeWork />
