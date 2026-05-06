2abe2b97c3ab966763a1882e95c4f263787feebd
Olha Chornei <Chornei8053@gmail.com>
2026-05-06 22:36:23 +0300

    google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/index.css';
 import { Montserrat } from 'next/font/google';
 import type { Metadata, Viewport } from 'next';
+import Script from 'next/script';
 
 const montserrat = Montserrat({
   subsets: ['latin', 'cyrillic'],
@@ -29,11 +30,25 @@ export default function RootLayout({
   return (
     <html lang="uk">
       <body className={montserrat.variable}>
+        {/* Google tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18143116631"
+          strategy="afterInteractive"
+        />
+
+        <Script id="gtag" strategy="afterInteractive">
+          {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'AW-18143116631');
+    `}
+        </Script>
         {children}
-        <script
-          type="module"
+        <Script
           src="https://static.rocket.new/rocket-web.js?_cfg=https%3A%2F%2Fbuddhasa1071back.builtwithrocket.new&_be=https%3A%2F%2Fapplication.rocket.new&_v=0.1.7"
-        ></script>
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
